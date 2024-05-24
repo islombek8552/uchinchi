768a6d8e9b762d19f0dd876e7995092f8974b624
islombek8552 <aaalyorbek@gmail.com>
2024-05-24 16:19:20 +0500

    eefwerf

---
+++ mainjs/my.js
@@ -0,0 +1,63 @@
+//1-masala
+
+//let a=+prompt("raqamni kiriting")
+//if(a>0){
+  //  let yegindi=parseInt(a+1)
+    //console.log(yegindi);
+//}
+//if(a<0){
+  //  let yegindi=parseInt(a*1)
+    //console.log(yegindi);
+//}
+
+//2-masala
+//let a=+prompt("raqamni kiriting")
+//if (a>0) {
+   // let yegindi=parseInt(a+1)
+   // console.log(yegindi);
+//}//
+//if (a<0) {
+  //  let yegindi=parseInt(a+2)
+   /// console.log(yegindi);
+//}
+
+//3-masala
+//let a=+prompt("raqam")
+//if (a>0) {
+  // let yegindi=parseInt(a+1) 
+  //// console.log(yegindi);
+///}
+//if (a<0) {
+  ///  let yegindi=parseInt(a+2) 
+  // console.log(yegindi);
+//}
+////if (a>-1,a<1) {
+   /// let yegindi=parseInt("10")
+   /// console.log(yegindi);
+//}
+
+//4-masala
+
+let a=+prompt("sonni kiriting")
+let b=+prompt("yana kiriting")
+let c=+prompt("yana kiriting")
+if (a,b,s>0) {
+   let musbat=parseInt
+   console.log(musbat);
+   
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
